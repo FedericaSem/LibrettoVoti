9a8963046809c8c4654226b93f8b332ad24ae5ba
Federica <Federica@DESKTOP-EFG654G>
2019-03-19 10:58:36 +0000

    completato punto 2

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -24,4 +24,22 @@ public class Libretto {
 	public void add(Voto v) {
 		voti.add(v);
 	}
+	
+	/**
+	 * seleziona il sottoinsieme di voti che hanno il punteggio specificato
+	 * @param punti punteggio da ricercare
+	 * @return lista di {@link Voto} aventi quel punteggio (eventalmente vuota)
+	 */
+	public List<Voto> cercaVoti(int punti){
+		List<Voto> result=new ArrayList<Voto>();
+		
+		for(Voto v: this.voti) {
+			if(v.getPunti()==punti) {
+				result.add(v);
+			}
+		}
+		return result;
+
+	}
+	
 }
